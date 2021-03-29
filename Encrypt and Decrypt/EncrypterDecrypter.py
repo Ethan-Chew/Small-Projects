31eaba39d278894fad2130c36df1a5ad9fd0f3ec
Ethan Chew <ethanchewmh@gmail.com>
2021-03-29 13:18:47 +0800

    Updated some code, Added new section

--- Encrypt and Decrypt/EncrypterDecrypter.py
+++ Encrypt and Decrypt/EncrypterDecrypter.py
@@ -1,20 +1,27 @@
 cypher = {"a":"4bB*","b":"a3Vf","c":"B3a*","d":"1bHn","e":"a*(6","f":"kBH7","g":"bh52","h":"aP3b","i":"a(2h","j":"pI0a","k":"t(A*","l":"aBeL","m":"R*(n","n":"2bA0","o":"Ar9b","p":"a5h*","q":")(*)","r":"b9a)","s":"2jx9","t":"r94k","u":"2j0q","v":"4a*(","w":"4nq3","x":"24(*","y":"*(yz","z":"lM84"}
+specialChar = ['!', '?', ',', '&', "'", '"']
 
 print("Input 'c' for Cypher, and 'd' for Decypher")
 cdChoice = input("Input: ")
 
 if cdChoice.lower() == 'c':
-    print("Selected Cypher")
+    print("Selected Cypher, () CANNOT BE USED IN YOUR CYPHER TEXT")
     cypherText = input("Enter text: ")
     cypherText = cypherText.lower()
 
     words = cypherText.split()
     cypherWordsArr = []
     encryptedText = ""
+    positionOfSpecialChar = {}
 
     # Store each word in array
     for word in words:
         cypherWordsArr.append(word)
+        # Check for Special Characters
+        for i in range(len(specialChar)):
+            for a in range(len(cypherWordsArr)):
+                if specialChar[i] in cypherWordsArr[a]:
+                    positionOfSpecialChar[cypherWordsArr[a].find(specialChar[i])*4] = specialChar[i] # Times 4 because each alphabet encrypted is 4 letters long
 
     # Encrypt Words
     for wordIndex in range(len(cypherWordsArr)):
@@ -24,13 +31,31 @@ if cdChoice.lower() == 'c':
 
         encryptedText += " "
 
-    print(encryptedText)
+    encryptedText = encryptedText + "\b"
 
-elif (cdChoice.lower() == 'd'):
+    # Check for Special Char
+    if not positionOfSpecialChar:
+        print("No Special Characters")
+    else:
+        positions = list(positionOfSpecialChar.keys())
+        symbols = list(positionOfSpecialChar.values())
+        lengthEncryptedText = len(encryptedText)
+
+        if len(positionOfSpecialChar) == 1:
+            encryptedText = encryptedText[:positions[0]] + symbols[0] + encryptedText[lengthEncryptedText:]
+        else:
+            for i in range(len(positionOfSpecialChar)):
+                print(len(positionOfSpecialChar))
+                encryptedText = encryptedText[:positions[i]] + symbols[i] + encryptedText[lengthEncryptedText - i:]
+
+    print("Cipher Text: "+encryptedText)
+
+elif cdChoice.lower() == 'd':
     print("Selected Decipher")
     decypherText = input("Paste Cryptic text: ")
 
     encryptedTextArr = []
+    positionOfSpecialChar = {}
     encryptedText = ""
     decryptedText = ""
 
@@ -39,6 +64,11 @@ elif (cdChoice.lower() == 'd'):
     # Store words in array
     for word in words:
         encryptedTextArr.append(word)
+        # Check for Special Characters
+        for i in range(len(specialChar)):
+            for a in range(len(encryptedTextArr)):
+                if specialChar[i] in encryptedTextArr[a]:
+                    positionOfSpecialChar[encryptedTextArr[a].find(specialChar[i])] = specialChar[i] # Times 4 because each alphabet encrypted is 4 letters long
 
     # Decrypt
     ## Explaination
@@ -57,7 +87,27 @@ elif (cdChoice.lower() == 'd'):
 
                 decrypt = cypherValues.index(alphabetCode)
                 decryptedText += cypherKey[decrypt]
+            elif alphabetCode in specialChar:
+                positionOfSpecialChar[i] = alphabetCode
         decryptedText += " "
+
+    decryptedText = decryptedText + "\b"
+
+    # Check for Special Char
+    if not positionOfSpecialChar:
+        print("No Special Characters")
+    else:
+        positions = list(positionOfSpecialChar.keys())
+        symbols = list(positionOfSpecialChar.values())
+        lengthEncryptedText = len(encryptedText)
+
+        if len(positionOfSpecialChar) == 1:
+            position = int((positions[0]/4))
+            encryptedText = encryptedText[:position] + symbols[0] + encryptedText[lengthEncryptedText - 0:]
+        else:
+            for i in range(len(positionOfSpecialChar)):
+                position = int(positions[i]/4)
+                encryptedText = encryptedText[:position] + symbols[i] + encryptedText[lengthEncryptedText - i:]
 
     print(decryptedText)
 else:
